fd3081d4fb87a8280b7e8c291e326831fd35ebd8
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2014-12-22 19:45:28 +0100

    Use Vcsrepot do download composer.json

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,29 +1,23 @@
 class cachedeps {
 
-    file { '/tmp/symfony-standard':
-        ensure  => directory,
-        mode    => 0777,
-        owner   => 'vagrant',
-        group   => 'vagrant'
-    }
-
-    exec { 'cachedeps-get-composer-json':
-        command => "wget -O /tmp/symfony-standard/composer.json https://raw.githubusercontent.com/symfony/symfony-standard/2.6/composer.json",
-        path    => '/usr/bin:/bin:/usr/sbin:/sbin',
-        onlyif  => 'test ! -f /tmp/symfony-standard/composer.json',
-        require => File['/tmp/symfony-standard']
+    vcsrepo { '/tmp/symfony-standard':
+        provider => 'git',
+        source   => 'https://github.com/symfony/symfony-standard.git',
+        user     => 'vagrant',
+        revision => 'v2.6.1',
+        depth    => 1
     }
 
     exec { 'cachedeps-composer-install':
-        command     => "composer install",
+        command     => 'composer install -q',
         environment => 'HOME=/home/vagrant',
-        cwd         => "/tmp/symfony-standard",
+        cwd         => '/tmp/symfony-standard',
         path        => '/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin',
-        require     => [File['/tmp/symfony-standard'], Exec['cachedeps-get-composer-json']]
+        require     => [Vcsrepo['/tmp/symfony-standard']]
     }
 
     exec { 'cachedeps-rm':
-        command => "rm -rf /tmp/symfony-standard",
+        command => 'rm -rf /tmp/symfony-standard',
         path    => '/usr/bin:/bin:/usr/sbin:/sbin',
         require => [Exec['cachedeps-composer-install']]
     }
